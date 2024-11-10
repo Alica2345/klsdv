a802da66473626ff29ea3b58773da095160d176e
Alica2345 <alisa2010sun@gmail.com>
2024-11-10 13:50:03 +0200

    add lessons

--- dz1.py
+++ dz1.py
@@ -7,3 +7,12 @@ class Rectangle :
 
 product1 = Rectangle
 product1.description()
+class Rectangle :
+    def __init__(self):
+        self.width = 20
+        self.hight = 30
+    def description(self):
+        print(self.width,self.hight)
+
+product1 = Rectangle
+product1.description()
